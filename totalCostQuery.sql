aeda4f235f7e7cdd56cf30b3ef2eb5b96e127c7f
Steffen Holanger <holanger.steffen@gmail.com>
2019-10-31 15:35:00 +0100

    making it nicer

--- totalCostQuery.sql
+++ totalCostQuery.sql
@@ -1,6 +1,5 @@
-select totalhourlycost * plansruntimeDays as totalcost, activity from
-(select distinct sum(hourlycost) as totalhourlycost from plans
-inner join planemployee p on plans.id = p.planid
-left join  employees e on p.employeeid = e.id group by activity) as wages,
-(select (enddate - startdate) plansruntimeDays, activity from plans) as days
-group by activity, totalhourlycost * plansruntimeDays
+(select sum(hourlycost) * (enddate - startdate) as cost, pl.id as plansID
+from plans pl
+         inner join planemployee p on pl.id = p.planid
+         inner join employees e on p.employeeid = e.id
+group by pl.id)
